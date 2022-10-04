6f688dd7bf9cbfc7d704bd7e683183cfa36ddb90
uday <itsudayy@gmail.com>
2022-10-04 08:44:48 +0530

    basic routing

--- server.js
+++ server.js
@@ -1,21 +1,61 @@
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
+const { response } = require("express");
+const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
+const API_KEY = process.env.API_KEY;
+
 const app = express();
 
-var url =
-  "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo";
+app.use(express.urlencoded({ extended: true }));
 
-axios
-  .get(url)
-  .then((res) => {
-    let metaData = res.data["Meta Data"];
-    console.log(metaData);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: API_KEY,
+  },
+});
 
-// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/top_rated", (req, res) => {
+  // check if the value is not given/undefined/0
+  let page = parseInt(req.query.page);
+  if (isNaN(page)) {
+    page = 1;
+  }
+
+  // Makes the request to MOVIES_DB
+  instance
+    .get("/movie/top_rated", {
+      params: {
+        page: page,
+      },
+    })
+    .then((response) => {
+      let names = "";
+
+      // gets the actual data from the request made
+      let data = response.data.results;
+      // res.send(data);
+
+      for (let i = 0; i < data.length; i++) {
+        let title = data[i]["original_title"];
+        names += `<h1>${title}</h1>`;
+      }
+
+      res.send(names);
+    })
+    .catch((err) => {
+      res.send("Error" + err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to PORT ${PORT}`);
+});
