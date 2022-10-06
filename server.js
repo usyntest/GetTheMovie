318705ddda9f8d820aac0ab094d3c9e89a50055e
uday <itsudayy@gmail.com>
2022-10-06 07:40:05 +0530

    indiviual movie details api endpoint made

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
-const { response } = require("express");
 const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -12,8 +11,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
+const baseURL = "https://api.themoviedb.org/3";
+
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
+  baseURL,
   params: {
     api_key: API_KEY,
   },
@@ -31,28 +32,53 @@ app.get("/top_rated", (req, res) => {
   }
 
   // Makes the request to MOVIES_DB
+
   instance
     .get("/movie/top_rated", {
       params: {
-        page: page,
+        page,
       },
     })
     .then((response) => {
-      let names = "";
-
       // gets the actual data from the request made
-      let data = response.data.results;
+      let { results } = response.data;
       // res.send(data);
 
-      for (let i = 0; i < data.length; i++) {
-        let title = data[i]["original_title"];
-        names += `<h1>${title}</h1>`;
+      for (let i = 0; i < results.length; i++) {
+        let title = results[i]["original_title"];
+        let link = results[i].id;
+        res.write(`<h1>${title}</h1>`);
+        res.write(
+          `<a target=_blank href="/movie/${link}">${results[i].id}</a>`
+        );
       }
 
-      res.send(names);
+      res.send();
     })
     .catch((err) => {
-      res.send("Error" + err);
+      res.send("Error: " + err);
+    });
+});
+
+// This api endpoint is to get a specific movie
+app.get("/movie/:id", (req, res) => {
+  instance
+    .get(`/movie/${req.params.id}`)
+    .then((response) => {
+      let { data } = response;
+
+      let img_path = "https://image.tmdb.org/t/p/w500";
+
+      console.log(img_path + data.poster_path);
+
+      res.send(
+        `<img src=${img_path + data.poster_path} alt="${
+          data.original_title
+        }"><h1>${data.original_title}</h1><p>${data.overview}</p>`
+      );
+    })
+    .catch((err) => {
+      res.send("Error: " + err);
     });
 });
 
